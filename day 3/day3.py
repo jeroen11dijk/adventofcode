ab490136001eb054b638f018b873519083de3263
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-12-03 11:54:35 +0100

    Day 3 completed

---
+++ day 3/day3.py
@@ -0,0 +1,38 @@
+import time
+import math
+
+
+def puzzle1():
+    res = 0
+    with open('day3.txt') as f:
+        f.__next__()
+        for i, line in enumerate(f):
+            res += line[((i + 1) * 3) % (len(line.strip('\n')))] == '#'
+    return res
+
+
+def puzzle2():
+    res = 5 * [0]
+    counter = 1
+    with open('day3.txt') as f:
+        f.__next__()
+        for i, line in enumerate(f):
+            res[0] += line[(i + 1) % (len(line.strip('\n')))] == '#'
+            res[1] += line[((i + 1) * 3) % (len(line.strip('\n')))] == '#'
+            res[2] += line[((i + 1) * 5) % (len(line.strip('\n')))] == '#'
+            res[3] += line[((i + 1) * 7) % (len(line.strip('\n')))] == '#'
+            if i % 2 == 1:
+                res[4] += line[(counter) % (len(line.strip('\n')))] == '#'
+                counter += 1
+    return math.prod(res)
+
+
+if __name__ == '__main__':
+    start_puzzle1 = int(round(time.time() * 1000))
+    res_puzzle1 = puzzle1()
+    end_puzzle1 = int(round(time.time() * 1000))
+    print("Puzzle 1 : " + str(res_puzzle1) + ". Took: " + str((end_puzzle1 - start_puzzle1)))
+    start_puzzle2 = int(round(time.time() * 1000))
+    res_puzzle2 = puzzle2()
+    end_puzzle2 = int(round(time.time() * 1000))
+    print("Puzzle 1 : " + str(res_puzzle2) + ". Took: " + str((end_puzzle2 - start_puzzle2)))
