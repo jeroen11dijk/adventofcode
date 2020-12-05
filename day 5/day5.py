1fe3519618a854e8b5e22536b67007b65df58020
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-12-05 12:48:01 +0100

    day 5

---
+++ day 5/day5.py
@@ -0,0 +1,40 @@
+import time
+
+
+def convertToNumber(string, upper):
+    res = 0
+    for i, char in enumerate(reversed(string[0:7])):
+        if char == upper:
+            res += 2 ** i
+    return res
+
+
+def puzzle1():
+    res = 0
+    with open('day5.txt') as f:
+        for line in f:
+            seatId = convertToNumber(line[0:7], 'B') * 8 + convertToNumber(line[7:10], 'R')
+            res = max(res, seatId)
+    return res
+
+
+def puzzle2():
+    seatIds = set()
+    with open('day5.txt') as f:
+        for line in f:
+            seatId = convertToNumber(line[0:7], 'B') * 8 + convertToNumber(line[7:10], 'R')
+            seatIds.add(seatId)
+    for i in range(min(seatIds), max(seatIds)):
+        if i not in seatIds:
+            return i
+
+
+if __name__ == '__main__':
+    start_puzzle1 = int(round(time.time() * 1000))
+    res_puzzle1 = puzzle1()
+    end_puzzle1 = int(round(time.time() * 1000))
+    print("Puzzle 1 : " + str(res_puzzle1) + ". Took: " + str((end_puzzle1 - start_puzzle1)))
+    start_puzzle2 = int(round(time.time() * 1000))
+    res_puzzle2 = puzzle2()
+    end_puzzle2 = int(round(time.time() * 1000))
+    print("Puzzle 1 : " + str(res_puzzle2) + ". Took: " + str((end_puzzle2 - start_puzzle2)))
