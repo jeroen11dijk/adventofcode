8fbfe16edf74074a9593d1752cae3b22f8dd91f0
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-12-14 13:23:55 +0100

    Updates

--- day14/day14.py
+++ day14/day14.py
@@ -6,21 +6,17 @@ def puzzle1():
     mask = ""
     memory = {}
     for line in open('day14.txt'):
-        command, value = line.split('=')
-        command = command.strip()
-        value = value.strip()
+        command, value = line.split(' = ')
         if command == "mask":
             mask = value
         else:
-            value = bin(int(value))[2:].zfill(36)
-            memoryaddress = command[4:-1]
             masked_value = ""
-            for i, bit in enumerate(value):
+            for i, bit in enumerate(bin(int(value))[2:].zfill(36)):
                 if mask[i] != 'X':
                     masked_value += mask[i]
                 else:
                     masked_value += bit
-            memory[memoryaddress] = int(masked_value, 2)
+            memory[command[4:-1]] = int(masked_value, 2)
     return sum(memory.values())
 
 
@@ -28,30 +24,27 @@ def puzzle2():
     mask = ""
     memory = {}
     for line in open('day14.txt'):
-        command, value = line.split('=')
-        command = command.strip()
+        command, value = line.split(' = ')
         if command == "mask":
             mask = value.strip()
         else:
             memoryaddress = bin(int(command[4:-1]))[2:].zfill(36)
             masked_memoryaddress = ""
             for i, bit in enumerate(memoryaddress):
-                if mask[i] == 'X':
-                    masked_memoryaddress += "X"
-                elif mask[i] == "1":
-                    masked_memoryaddress += "1"
+                if mask[i] == 'X' or mask[i] == "1":
+                    masked_memoryaddress += mask[i]
                 else:
                     masked_memoryaddress += bit
-            floating_memories = [masked_memoryaddress]
+            memories = [masked_memoryaddress]
             for i, bit in enumerate(masked_memoryaddress):
                 new_memories = []
                 if bit == "X":
-                    for floating_memory in floating_memories:
-                        new_memories.append(floating_memory[:i] + "0" + floating_memory[i+1:])
-                        new_memories.append(floating_memory[:i] + "1" + floating_memory[i+1:])
-                    floating_memories = new_memories
-            for floating_memory_address in floating_memories:
-                memory[int(floating_memory_address, 2)] = int(value.strip())
+                    for floating_memory in memories:
+                        new_memories.append(floating_memory[:i] + "0" + floating_memory[i + 1:])
+                        new_memories.append(floating_memory[:i] + "1" + floating_memory[i + 1:])
+                    memories = new_memories
+            for floating_memory_address in memories:
+                memory[int(floating_memory_address, 2)] = int(value)
     return sum(memory.values())
 
 
