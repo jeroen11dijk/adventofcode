9114aec47577e5fdc4bcf76269c7b35e4c0e8eec
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-12-16 20:41:50 +0100

    Cleanup

--- day16/day16.py
+++ day16/day16.py
@@ -4,8 +4,8 @@ from collections import defaultdict
 
 
 def puzzle1():
-    valid = set()
     parts = open("day16.txt").read().split("\n\n")
+    valid = set()
     for line in parts[0].split("\n"):
         for span in line.split(': ')[1].split(' or '):
             for i in range(int(span.split("-")[0]), int(span.split("-")[1]) + 1):
@@ -16,47 +16,40 @@ def puzzle1():
 def invalid_lines():
     valid = set()
     res = []
-    for line in open('ids.txt'):
-        if line.split() == "your ticket:":
-            break
-        _, ranges = line.split(': ')
-        first, second = ranges.split(' or ')
-        for i in range(int(first.split("-")[0]), int(first.split("-")[1]) + 1):
-            valid.add(i)
-        for i in range(int(second.split("-")[0]), int(second.split("-")[1]) + 1):
-            valid.add(i)
-    for line_number, line in enumerate(open("tickets.txt")):
-        line = list(map(int, line.split(",")))
-        for i in line:
+    parts = open("day16.txt").read().split("\n\n")
+    for line in parts[0].split("\n"):
+        for span in line.split(': ')[1].split(' or '):
+            for i in range(int(span.split("-")[0]), int(span.split("-")[1]) + 1):
+                valid.add(i)
+    for line in parts[2].split("\n")[1:]:
+        for i in list(map(int, line.split(","))):
             if i not in valid:
-                res.append(line)
+                res.append(list(map(int, line.split(","))))
     return res
 
 
 def puzzle2():
-    valid = {}
-    skip = invalid_lines()
-    for line in open('ids.txt'):
+    parts = open("day16.txt").read().split("\n\n")
+    valid_per_field = defaultdict(set)
+    valid = set()
+    # Get all the valid numbers for every field
+    for line in parts[0].split("\n"):
         field, ranges = line.split(': ')
-        first, second = ranges.split(' or ')
-        valid[field] = set()
-        for i in range(int(first.split("-")[0]), int(first.split("-")[1]) + 1):
-            valid[field].add(i)
-        for i in range(int(second.split("-")[0]), int(second.split("-")[1]) + 1):
-            valid[field].add(i)
-    lijsten = []
-    for i in range(len(open("tickets.txt").readlines()[0].split(','))):
-        lijsten.append(set())
-    for line in open("tickets.txt"):
+        for span in ranges.split(' or '):
+            for i in range(int(span.split("-")[0]), int(span.split("-")[1]) + 1):
+                valid_per_field[field].add(i)
+                valid.add(i)
+    values_per_column = [set() for _ in range(len(parts[2].split("\n")[1].split(",")))]
+    for line in parts[2].split("\n")[1:]:
         line = list(map(int, line.split(",")))
-        if line not in skip:
+        if all([value in valid for value in line]):
             for i, value in enumerate(line):
-                lijsten[i].add(value)
+                values_per_column[i].add(value)
     assigned = set()
     possibilities = defaultdict(list)
-    for index, lijst in enumerate(lijsten):
-        for key in valid.keys():
-            if all(value in valid[key] for value in lijst) and key not in assigned:
+    for index, column in enumerate(values_per_column):
+        for key in valid_per_field.keys():
+            if all(value in valid_per_field[key] for value in column) and key not in assigned:
                 possibilities[index].append(key)
         if len(possibilities[index]) == 1:
             assigned.add(possibilities[index][0])
@@ -68,7 +61,7 @@ def puzzle2():
                         possibilities[key].remove(value)
             if len(possibilities[key]) == 1:
                 assigned.add(possibilities[key][0])
-    my_ticket = [191, 61, 149, 157, 79, 197, 67, 139, 59, 71, 163, 53, 73, 137, 167, 173, 193, 151, 181, 179]
+    my_ticket = list(map(int, parts[1].split("\n")[1].split(",")))
     interested_in = ["departure location", "departure station", "departure platform", "departure track",
                      "departure date", "departure time"]
     res = []
