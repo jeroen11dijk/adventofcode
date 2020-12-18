6c8ea9bf8ecfa93f47be57c3f12084034663bc49
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-12-18 12:51:36 +0100

    Sample parser for day 18

---
+++ day18/day18.py
@@ -0,0 +1,50 @@
+import time
+import parser
+
+def solve(expression):
+    res = 0
+    add = True
+    in_bracket = False
+    bracket = []
+    for x in expression:
+        if in_bracket:
+            if ")" in x:
+                bracket.append(x.replace(")", "", 1))
+                res = res + solve(bracket) if add else res * solve(bracket)
+                in_bracket = False
+                bracket = []
+            else:
+                bracket.append(x)
+        else:
+            if x == "+":
+                add = True
+            elif x == "*":
+                add = False
+            elif "(" in x:
+                in_bracket = True
+                bracket.append(x.replace("(", "", 1))
+            else:
+                res = res + int(x) if add else res * int(x)
+    return res
+
+
+def puzzle1():
+    res = 0
+    for line in open("day18.txt"):
+        res += solve(line.split())
+    return res
+
+
+def puzzle2():
+    return 0
+
+
+if __name__ == '__main__':
+    start_puzzle1 = int(round(time.time() * 1000))
+    res_puzzle1 = puzzle1()
+    end_puzzle1 = int(round(time.time() * 1000))
+    print("Puzzle 1: " + str(res_puzzle1) + ". Took: " + str((end_puzzle1 - start_puzzle1)))
+    start_puzzle2 = int(round(time.time() * 1000))
+    res_puzzle2 = puzzle2()
+    end_puzzle2 = int(round(time.time() * 1000))
+    print("Puzzle 2: " + str(res_puzzle2) + ". Took: " + str((end_puzzle2 - start_puzzle2)))
