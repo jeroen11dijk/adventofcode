55d81361ed8fc74275dc588c681dd22a8e64e9e3
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-12-22 13:21:41 +0100

    And puzzle 2

--- day22/day22.py
+++ day22/day22.py
@@ -6,11 +6,11 @@ from lark.lark import Lark
 
 def puzzle1():
     players = open("day22.txt").read().split('\n\n')
-    player1 = players[0].split("\n")[1:]
-    player2 = players[1].split("\n")[1:]
+    player1 = list(map(int, players[0].split("\n")[1:]))
+    player2 = list(map(int, players[1].split("\n")[1:]))
     while len(player1) > 0 and len(player2) > 0:
-        card1 = int(player1.pop(0))
-        card2 = int(player2.pop(0))
+        card1 = player1.pop(0)
+        card2 = player2.pop(0)
         if card1 > card2:
             player1.append(card1)
             player1.append(card2)
@@ -18,13 +18,45 @@ def puzzle1():
             player2.append(card2)
             player2.append(card1)
     if len(player1) > 0:
-        return sum([(i + 1) * int(val) for i, val in enumerate(reversed(player1))])
+        return sum([(i + 1) * val for i, val in enumerate(reversed(player1))])
     if len(player2) > 0:
-        return sum([(i + 1) * int(val) for i, val in enumerate(reversed(player2))])
+        return sum([(i + 1) * val for i, val in enumerate(reversed(player2))])
+
+
+def play_game(player1, player2):
+    seen = set()
+    while len(player1) > 0 and len(player2) > 0:
+        card_state = (tuple(player1), tuple(player2))
+        if card_state in seen:
+            return True, sum([(i + 1) * val for i, val in enumerate(reversed(player1))])
+        seen.add(card_state)
+        card1 = player1.pop(0)
+        card2 = player2.pop(0)
+        if card1 <= len(player1) and card2 <= len(player2):
+            winner, _ = play_game(player1[:card1], player2[:card2])
+            if winner:
+                player1.append(card1)
+                player1.append(card2)
+            else:
+                player2.append(card2)
+                player2.append(card1)
+        elif card1 > card2:
+            player1.append(card1)
+            player1.append(card2)
+        else:
+            player2.append(card2)
+            player2.append(card1)
+    if len(player1) > 0:
+        return True, sum([(i + 1) * val for i, val in enumerate(reversed(player1))])
+    if len(player2) > 0:
+        return False, sum([(i + 1) * val for i, val in enumerate(reversed(player2))])
 
 
 def puzzle2():
-    return 0
+    players = open("day22.txt").read().split('\n\n')
+    player1 = list(map(int, players[0].split("\n")[1:]))
+    player2 = list(map(int, players[1].split("\n")[1:]))
+    return play_game(player1, player2)[1]
 
 
 if __name__ == '__main__':
