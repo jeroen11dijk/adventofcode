6cfd174dfe676383f3991f4e05eb997add5f1e74
Jeroen van Dijk <jeroen11dijk@gmail.com>
2020-12-27 19:12:41 +0100

    Last commit

--- day24/day24.py
+++ day24/day24.py
@@ -26,8 +26,43 @@ def puzzle1():
     return sum(1 for k, v in flips.items() if v % 2 == 1)
 
 
+def step(flips):
+    neighbours = collections.defaultdict(int)
+    for k, v in flips.items():
+        if v % 2 == 0:
+            continue
+        for adj in grid.values():
+            pt = (k[0] + adj[0], k[1] + adj[1])
+            neighbours[pt] += 1
+
+    newflips = {}
+    for k, v in flips.items():
+        if v % 2 == 1:
+            if neighbours.get(k, 0) not in (1, 2):
+                # flip to white
+                pass
+            else:
+                newflips[k] = 1
+    for k, v in neighbours.items():
+        if v == 2 and flips.get(k, 0) % 2 == 0:
+            newflips[k] = 1
+    return newflips
+
+
 def puzzle2():
-    return 0
+    lines = open("day24.txt").read().split('\n')
+    flips = collections.defaultdict(int)
+    for line in lines:
+        dirs = re.findall('e|se|sw|w|nw|ne', line)
+        x, y = 0, 0
+        for d in dirs:
+            char = grid[d]
+            x += char[0]
+            y += char[1]
+        flips[(x, y)] += 1
+    for i in range(100):
+        flips = step(flips)
+    return sum(1 for k, v in flips.items() if v % 2 == 1)
 
 
 if __name__ == '__main__':
